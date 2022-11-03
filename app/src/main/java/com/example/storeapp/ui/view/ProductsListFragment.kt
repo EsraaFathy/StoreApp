7e5b3f1de55f9f13ff28ad8dc5c94c53b808fdad
Esraa Fathy <fesraa240@gmail.com>
2022-11-03 18:16:26 +0200

    show recycler

--- app/src/main/java/com/example/storeapp/ui/view/ProductsListFragment.kt
+++ app/src/main/java/com/example/storeapp/ui/view/ProductsListFragment.kt
@@ -6,7 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.storeapp.R
 import com.example.storeapp.base.BaseFragment
 import com.example.storeapp.base.ViewModelFactory
@@ -14,9 +16,13 @@ import com.example.storeapp.data.remote.RemoteDataSourceImp
 import com.example.storeapp.data.remote.RepositoryImp
 import com.example.storeapp.databinding.FragmentProductsListBinding
 import com.example.storeapp.model.ProductList
+import androidx.recyclerview.widget.LinearLayoutManager
 
 
-class ProductsListFragment : BaseFragment() {
+
+
+
+class ProductsListFragment : BaseFragment(),ItemClick {
     private lateinit var binding: FragmentProductsListBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -46,7 +52,14 @@ class ProductsListFragment : BaseFragment() {
     }
 
     private fun initRecyclerView(list: ProductList) {
+        binding.productsListRv.apply {
+            this.adapter = ProductsAdapter(list,this@ProductsListFragment)
+            this.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+        }
+    }
 
+    override fun onProductClick(product: ProductList.ProductListItem) {
+        Toast.makeText(requireContext(),"",Toast.LENGTH_SHORT).show()
     }
 
 }
